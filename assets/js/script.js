c72f468ccd6b12c71d837875e7b1b540c1497819
Ansonldoublee <ansonldoublee@gmail.com>
2024-06-17 13:48:51 +1000

    Initial commit - homepage HTML and CSS with images

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// This is for the tab filtering
+document.addEventListener('DOMContentLoaded', (event) => {
+    const tabButtons = document.querySelectorAll('.tab-button');
+    const allCards = document.querySelectorAll('.project-card');
+
+    function filterCards(category) {
+        allCards.forEach(card => {
+            const cardId = card.querySelector('.project-card-img').id;
+            if (category === 'all' || category.includes(cardId)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+
+    tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Remove active class from all buttons
+            tabButtons.forEach(btn => btn.classList.remove('active'));
+            // Add active class to the clicked button
+            button.classList.add('active');
+            // Filter cards based on the selected category
+            const category = button.getAttribute('data-category').split(' ');
+            filterCards(category.includes('all') ? 'all' : category);
+        });
+    });
+
+    // Show all cards by default
+    filterCards('all');
+});
